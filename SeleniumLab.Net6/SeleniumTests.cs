556bbbcff4e99e440ce47837098621f2e3ddd1d1
Daniil Palii <daniil.palii@proton.me>
2023-02-28 19:41:02 +0100

    add chrome tests

---
+++ SeleniumLab.Net6/SeleniumTests.cs
@@ -0,0 +1,60 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+
+namespace SeleniumDocs.GettingStarted
+{
+    [TestFixture]
+    public class SeleniumTests
+    {
+        [Test]
+        public void ChromeSession()
+        {
+            IWebDriver driver = new ChromeDriver("SeleniumDrivers");
+
+            driver.Navigate().GoToUrl("https://www.selenium.dev/selenium/web/web-form.html");
+
+            var title = driver.Title;
+            Assert.AreEqual("Web form", title);
+
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromMilliseconds(500);
+
+            var textBox = driver.FindElement(By.Name("my-text"));
+            var submitButton = driver.FindElement(By.TagName("button"));
+
+            textBox.SendKeys("Selenium");
+            submitButton.Click();
+
+            var message = driver.FindElement(By.Id("message"));
+            var value = message.Text;
+            Assert.AreEqual("Received!", value);
+
+            driver.Quit();
+        }
+
+        [Test]
+        public void EdgeSession()
+        {
+            IWebDriver driver = new EdgeDriver("SeleniumDrivers");
+
+            driver.Navigate().GoToUrl("https://www.selenium.dev/selenium/web/web-form.html");
+
+            var title = driver.Title;
+            Assert.AreEqual("Web form", title);
+
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromMilliseconds(500);
+
+            var textBox = driver.FindElement(By.Name("my-text"));
+            var submitButton = driver.FindElement(By.TagName("button"));
+
+            textBox.SendKeys("Selenium");
+            submitButton.Click();
+
+            var message = driver.FindElement(By.Id("message"));
+            var value = message.Text;
+            Assert.AreEqual("Received!", value);
+
+            driver.Quit();
+        }
+    }
+}
